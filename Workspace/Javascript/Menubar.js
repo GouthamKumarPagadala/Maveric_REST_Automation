5639fedcc7775a1fa567c3f2be3cc305437b4552
GouthamKumarPagadala <pagadalagoutham@gmail.com>
2017-11-05 16:05:51 +0000

    Finally Created the Side bar navigation
    Sidebar Navigator is developed inorder to navigate to the application

---
+++ Workspace/Javascript/Menubar.js
@@ -0,0 +1,29 @@
+function Menubar() {
+    var jsontext = '{"Menu_bar":["Dashboard","Swaggers","TestPlan","WorkFlow","TestLab","Run"]}';
+    var path = "./Database/Application/Menubar.json"
+    var txt = null;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            txt = this.responseText;
+            var getContact = JSON.parse(txt);
+            sidebar = document.getElementById("sidebar");
+            for (i = 0; i < getContact.Menu_bar.length; i++) {
+                //console.log(getContact.Menu_bar[i]);
+                div = document.createElement("div");
+                div.className = "sidebar-button";
+                div.style = "width:100%;  text-align:center; ";
+                lnk = document.createElement("a");
+                lnk.innerText = getContact.Menu_bar[i];
+                lnk.href = "#";
+                lnk.style = "color:#fff!important; ";
+                appended_div = sidebar.appendChild(div);
+                appended_div.appendChild(lnk);
+                sidebar.append("\n");
+            }
+        }
+    };
+    var j = {};
+    xhttp.open('GET', path, true);
+    xhttp.send();
+}
